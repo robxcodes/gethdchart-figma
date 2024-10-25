bce1ecf0aa8c14f57ff1cd1fab3257c044ca6ba2
Robert Sonny <robertxsonny@gmail.com>
2024-10-25 09:19:10 +0700

    update UI

--- src/code.ts
+++ src/code.ts
@@ -9,7 +9,7 @@ import { generate, HDValue } from './generator';
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, { height: 500, width: 300 });
+figma.showUI(__html__, { height: 500, width: 250 });
 
 figma.ui.onmessage = async (msg: { type: string, value: string }) => {
   if (msg.type === 'generate') {
